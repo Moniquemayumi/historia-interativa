8acae6288fea48162fa08e721ee7948c6f279bc2
Edsonneto3110 <edson.moura23@etepd.com>
2025-03-18 14:36:04 -0300

    Create atv.py

---
+++ atv.py
@@ -0,0 +1,6 @@
+nome = "Marah"
+idade = "19"
+poder = "agilidade"
+fraqueza = "sono"
+
+print(f"Era uma vez uma jovem chamada {nome}, de {idade} anos com o poder de {poder} ela tinha uma fraqueza que era o {fraqueza}, isso lhe atrapalhava a noite, mas mesmo assim ela conseguia realizar suas atividades a noite e ajudar as pessoas")
