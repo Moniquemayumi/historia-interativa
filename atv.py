45273eba90507b4e0ce99b0bd9b0a3641f19a429
umjotta <heliojuniorsly@gmail.com>
2025-03-19 15:47:23 -0300

    taylor swift canta mal!

--- atv.py
+++ atv.py
@@ -1,6 +1,6 @@
-nome = "Marah"
-idade = "19"
-poder = "agilidade"
-fraqueza = "sono"
+nome = "Taylor swift"
+idade = ""
+poder = "cantar mal"
+fraqueza = "amar demais"
 
-print(f"Era uma vez uma jovem chamada {nome}, de {idade} anos com o poder de {poder} ela tinha uma fraqueza que era o {fraqueza}, isso lhe atrapalhava a noite, mas mesmo assim ela conseguia realizar suas atividades a noite e ajudar as pessoas")
+print(f"Era uma vez uma jovem chamada {nome}, de {idade} anos com o poder de {poder},ela tinha uma fraqueza que era o {fraqueza}, isso lhe atrapalhava a noite, mas mesmo assim ela conseguia realizar suas atividades a noite e ajudar as pessoas")
